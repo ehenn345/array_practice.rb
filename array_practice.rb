c8ded43a066bf3778570a0bf69446c94da0b9c7c
erin <westonerin1@gmail.com>
2021-05-29 14:11:48 -0400

    more completed exercises

--- array_practice.rb
+++ array_practice.rb
@@ -5,19 +5,44 @@ word << "green"
 p word
 
 # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
-
+array = [ "f", "g", "e", "l"]
+array[1] = 3
+p array
 # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+array = [ 4, 5, 6, 7, 8]
+
+i = 0
+while i < array.length
+    p array[i]
+    i += 1
+end
+
 
 # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
-
+array = [5]
+array << 5
+array << 3
+array << 1
+p array
 # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
-
+array = [ "dog", "bone", "colar"]
+array[2] = array[2].upcase
+p array
 # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+array = ["john", "him", "todd"]
+i = 0
+while i < array.length
+    p array[i]
+    i += 1
+end
+
 
 # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
 
 # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
-
+array = [5, 4, 2, 1, 2]
+array[0] = array[0] * 10
+p array
 # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
 
 # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
---
+++ hash.rb
@@ -0,0 +1,34 @@
+# 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+
+person = {"first_name" => "joe", "last_name" => "johnson", "email" => "joej@johns.com"}
+p person["first_name"]
+p person["last_name"]
+p person["email"]
+# 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+people = [
+    {"first_name" => "koe", "last_name" => "kong"},
+    {"first_name" => "penut", "last_name" => "what"},
+    {"first_name" => "joe", "last_name" => "joi"}
+]
+
+p people[0]
+# 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
+
+food = {"chocolate" => 2, "beans" => 3, "water" => 2}
+food["hot cocoa"] = 3
+p food
+# 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+
+book = {"title" => "to be a dog", "author" => "hombre", "number_of_pages" => 33}
+p book["title"]
+# 5. Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+
+# 6. Make a hash to store 3 different states and their captitals. Then add a new state and capital and print the hash to see the result.
+
+# 7. Make a hash to store a laptop's brand, model, and year. Then print each attribute on separate lines.
+
+# 8. Make an array of hashes to store the brand and model for 3 different laptops. Then print out the second laptop's model.
+
+# 9. Make a hash to store definitions for 2 different words. Then add a new word and definition and print the hash to see the result.
+
+# 10. Make a hash to store a shirt's brand, color, and size. Then print each attribute on separate lines.
