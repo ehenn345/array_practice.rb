a0db1035988b1d587ea2d4e97e161d429d9be89b
erin <westonerin1@gmail.com>
2021-06-01 20:11:28 -0400

    more solutions solved

--- array_select.rb
+++ array_select.rb
@@ -29,8 +29,39 @@ p new_array
 #  3. Start with an array of hashes and create a new array with only the hashes with prices greater than 5 (from the :price key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}].
 
+
+array = [{name: "chair", price: 100}, 
+{name: "pencil", price: 1}, 
+{ name: "book", price: 4}]
+
+new_array = []
+
+array.each do |word|
+    if word[:price] < 5
+        new_array << word[:price]
+    end
+end
+
+p new_array
+
+
+
+
+
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
+
+array = [2, 4, 5, 1, 8, 9, 7]
+new_array = []
+
+array.each do |number|
+    if number.even? == true
+        new_array << number
+    end
+end
+
+p new_array
+
 
 #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 #     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
